90dfcda64272c670115364e4362db25e86e04516
nusrat-git <nusratjahanmithila66@gmail.com>
2022-11-23 23:02:22 +0600

    header and footer done

--- src/layout/Main/Main.js
+++ src/layout/Main/Main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../../shared/Footer/Footer';
+import NavigationBar from '../../shared/NavigationBar/NavigationBar';
 
 const Main = () => {
     return (
         <div>
-            
+            <NavigationBar></NavigationBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      backgroundImage: {
+        'hero-pattern': "url('/src/header.jpg')"
+      }
     },
   },
   plugins: [],
